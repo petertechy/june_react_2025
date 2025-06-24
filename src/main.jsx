ae6631a85a64bf805b28cd2f69565c5d489a8a61
petertechy <petertechy01@gmail.com>
2025-06-24 10:04:14 +0100

    Update main.jsx entry point

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+// import './index.css'
+import "./hello.css"
 import App from './App.jsx'
+import "../node_modules/bootstrap/dist/css/bootstrap.css"
+import "../node_modules/bootstrap/dist/js/bootstrap.js"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
