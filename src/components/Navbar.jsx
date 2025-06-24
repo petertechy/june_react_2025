38b0be61e61962fa55c8c0af092823cb905d2f57
petertechy <petertechy01@gmail.com>
2025-06-24 10:04:11 +0100

    Update Navbar.jsx component structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import style from "./Navbar.module.css"
 
 const Navbar = () => {
   return (
     <>
-        <h1>Navbar</h1>
-
+        <div className={style.container}>
+          <h1 className={style.logo}>Navbar</h1>
+        <ul>
+          <li>Home</li>
+          <li>Service</li>
+          <li>Contact</li>
+        </ul>
+        </div>
     </>
   )
 }
