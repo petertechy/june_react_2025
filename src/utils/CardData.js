24b1410dc0710aa82088166b4685f26109fccbcc
petertechy <petertechy01@gmail.com>
2025-06-30 16:24:24 +0100

    Add utils directory for utility functions

---
+++ src/utils/CardData.js
@@ -0,0 +1,47 @@
+import Image1 from "../assets/image1.png"
+import Image2 from "../assets/image2.png"
+
+export const cardItems = [
+  {
+    id: "1",
+    title: "Two-sided Business Cards",
+    price: "15,100",
+    desc: "hello everybody, I am business card",
+    img: Image1
+},
+{
+    id: "2",
+    title: "Two-sided Business Cards",
+    price: "15,100",
+    desc: "hello everybody, I am business card",
+    img: Image2
+},
+{
+    id: "3",
+    title: "Two-sided Business Cards",
+    price: "15,100",
+    desc: "hello everybody, I am business card",
+    img: Image1
+},
+{
+    id: "4",
+    title: "Two-sided Business Cards",
+    price: "15,100",
+    desc: "hello everybody, I am business card",
+    img: Image2
+},
+{
+    id: "5",
+    title: "Two-sided Business Cards",
+    price: "15,100",
+    desc: "hello everybody, I am business card",
+    img: Image1
+  },
+{
+    id: "6",
+    title: "Two-sided Business Cards",
+    price: "15,100",
+    desc: "hello everybody, I am business card",
+    img: Image2
+  },
+];
