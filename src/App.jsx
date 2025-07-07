2d10adab09f464a5d1871f192093434135ccb370
petertechy <petertechy01@gmail.com>
2025-07-07 11:52:56 +0100

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,11 +18,13 @@ import Navbar from './components/Navbar'
 import Banner from './components/Banner'
 import Card from './components/Card'
 import ClickEvent from './components/ClickEvent'
+import Product from './components/Product'
 
 const App = () => {
   return (
     <>
     <Navbar/>
+    <Product/>
     <ClickEvent/>
       <div className='mx-auto d-flex '>
         <h1 className='text-primary border'>Good Morning</h1>
