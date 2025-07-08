628689d917f1c8b9af3be2384951c805d7b6bc54
petertechy <petertechy01@gmail.com>
2025-07-08 21:47:14 +0100

    Update App.jsx with routing or layout changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,18 @@
-//React functional component - a function that returns JSX and exports it
-//JSX - Javascript + XML
-
-// function Azeem(){
-
-// }
-
-// const App = () => {
-//   return(
-//     <h1>Hello Students</h1>
-//   )
-// }
-
-// export default App;
-
 import React from 'react'
-import Navbar from './components/Navbar'
-import Banner from './components/Banner'
-import Card from './components/Card'
-import ClickEvent from './components/ClickEvent'
-import Product from './components/Product'
+import { Route, Routes } from 'react-router-dom'
+import About from './pages/About'
+import LandingPage from './pages/LandingPage'
+import NotFound from './pages/NotFound'
 
 const App = () => {
   return (
     <>
-    <Navbar/>
-    <Product/>
-    <ClickEvent/>
-      <div className='mx-auto d-flex '>
-        <h1 className='text-primary border'>Good Morning</h1>
-        <h1 className='text-danger border'>Good Afternoon</h1>
-      </div>
-      <Banner/>
-      <Card/>
+      <Routes>
+        <Route path='/' element={<LandingPage/>}/>
+        <Route path='/about' element={<About/>}/>
+
+        <Route path='*' element={<NotFound/>}/>
+      </Routes>
     </>
   )
 }
