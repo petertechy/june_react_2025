8c24ddadea40047daa19befcfd72101b6900d470
petertechy <petertechy01@gmail.com>
2025-07-08 21:48:19 +0100

    Add NotFound.jsx page for handling unknown routes

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+        <h1>You are on your own, what are you doing here!!!!</h1>
+    </div>
+  )
+}
+
+export default NotFound
