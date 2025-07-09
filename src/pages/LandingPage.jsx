6168560442e796744e83909003f7544c0551b029
petertechy <petertechy01@gmail.com>
2025-07-09 20:04:38 +0100

    Update LandingPage.jsx to reflect new product section changes

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Product from '../components/Product'
 
 const LandingPage = () => {
   return (
     <div>
       <h1>Oh my home my home, when shall i see my home, when shall i see my native land i will never forget my home</h1>
+      <Product/>
     </div>
   )
 }
