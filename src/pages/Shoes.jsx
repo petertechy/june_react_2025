2f4a000bff421b2762ef9a9c36c767dd75531366
petertechy <petertechy01@gmail.com>
2025-07-14 13:32:27 +0100

    Add Shoes.jsx page for footwear category

---
+++ src/pages/Shoes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Shoes = () => {
+  return (
+    <div className='my-2 p-2 '>
+        <h1 className='text-danger'>Shoes 👢 is available here</h1>
+    </div>
+  )
+}
+
+export default Shoes
