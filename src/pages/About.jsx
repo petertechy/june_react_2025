5fc212f35dadd0fc120b2635621d07b8eddab1fe
petertechy <petertechy01@gmail.com>
2025-07-14 13:31:05 +0100

    Update About.jsx with revised content or structure

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
 
 const About = () => {
+  const navigate = useNavigate()
+  const handleSubmit = ()=>{
+    alert("I have just been clicked")
+    navigate("/")
+  }
   return (
-    <div><h1>This is About Page</h1></div>
-  )
-}
+    <div>
+      <h1>This is About Page</h1>
+      <button className="btn btn-lg btn-primary my-3" onClick={handleSubmit}>Register</button>
+    </div>
+  );
+};
 
-export default About
+export default About;
