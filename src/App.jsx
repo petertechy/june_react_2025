346e6cb4ebf8f0f6111c373365e176d76426f077
petertechy <petertechy01@gmail.com>
2025-07-14 13:30:51 +0100

    Update App.jsx with routing or component changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,42 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import About from './pages/About'
-import LandingPage from './pages/LandingPage'
-import NotFound from './pages/NotFound'
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import LandingPage from "./pages/LandingPage";
+import NotFound from "./pages/NotFound";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Services from "./pages/Services";
+import UserProfile from "./pages/UserProfile";
+import Product2 from "./pages/Product2";
+import Fish from "./pages/Fish";
+import Meat from "./pages/Meat";
+import Shoes from "./pages/Shoes";
 
 const App = () => {
   return (
     <>
+      <Navbar />
       <Routes>
-        <Route path='/' element={<LandingPage/>}/>
-        <Route path='/about' element={<About/>}/>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<Navigate to="/" />} />
+        <Route path="about" element={<About />} />
+        <Route path="about-us" element={<Navigate to="/about" />} />
+        <Route path="service" element={<Services />} />
+        <Route path="profile/:username" element={<UserProfile />} />
 
-        <Route path='*' element={<NotFound/>}/>
+        <Route path="product" element={<Product2 />}>
+          <Route path="fish" element={<Fish />} />
+          <Route path="meat" element={<Meat/>} />
+          <Route path="shoe" element={<Shoes/>} />
+        </Route>
+
+
+        {/* <Route path="/product/fish" element={<Fish/>}/> */}
+        <Route path="*" element={<NotFound />} /> //Wildcard Route
       </Routes>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
