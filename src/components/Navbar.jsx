576c9e26de4f18e00f78b641df78b8d5190ecb88
petertechy <petertechy01@gmail.com>
2025-07-14 13:31:01 +0100

    Update Navbar.jsx for navigation adjustments

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import style from "./Navbar.module.css"
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
@@ -7,9 +8,9 @@ const Navbar = () => {
         <div className={style.container}>
           <h1 className={style.logo}>Navbar</h1>
         <ul>
-          <li>Home</li>
-          <li>Service</li>
-          <li>Contact</li>
+          <Link to="/"><li>Home</li></Link>
+          <Link to="/service"><li>Service</li></Link>
+          <Link to="/about"><li>About</li></Link>
         </ul>
         </div>
     </>
